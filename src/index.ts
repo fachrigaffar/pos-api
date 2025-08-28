3368bd3ee0dfdf31eef5e0a9f2fadecebb8e3ad2
Fachri Gaffar <fachrigaffar@gmail.com>
2025-08-28 11:32:29 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Running on Port${port}`);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello, POS Microservice!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
